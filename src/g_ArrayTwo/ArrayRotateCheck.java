5fe4377cde15ea626fb11ca2a70a6081a00eefb3
abhisheksaw <abhishekggs7@gmail.com>
2023-05-27 14:57:14 +0530

    Array II Part 2

---
+++ src/g_ArrayTwo/ArrayRotateCheck.java
@@ -0,0 +1,24 @@
+package g_ArrayTwo;
+
+public class ArrayRotateCheck {
+    public static void main(String[] args) {
+        int arr1[] = {5,6,1,2,3,4};
+        int answer1 = arrayRotateCheck(arr1);
+        System.out.println(answer1);
+        int arr2[] = {10,20,30,1};
+        int answer2 = arrayRotateCheck(arr2);
+        System.out.println(answer2);
+        int arr3[] = {};
+        int answer3 = arrayRotateCheck(arr3);
+        System.out.println(answer3);
+    }
+
+    private static int arrayRotateCheck(int[] arr) {
+            for(int i=0;i<arr.length-1;i++){
+                if(arr[i]>arr[i+1]){
+                   return i+1;
+                }
+            }
+            return 0;
+    }
+}
---
+++ src/g_ArrayTwo/SumOfTwoArray.java
@@ -0,0 +1,51 @@
+package g_ArrayTwo;
+
+import e_ArrayOne.TakeInputAndPrint;
+
+public class SumOfTwoArray {
+    public static void sumOfTwoArrays(int arr1[], int arr2[], int output[]) {
+        int i = arr1.length-1;
+        int j = arr2.length-1;
+        int k = Math.max(arr1.length,arr2.length);
+        int sum = 0;
+        int carry = 0;
+        while(i>=0 &&j>=0){
+            sum = arr1[i] + arr2[j] + carry;
+            output[k] = sum%10;
+            carry = sum/10;
+            i--;
+            j--;
+            k--;
+        }
+        while(i>=0){
+            sum = arr1[i] + carry;
+            output[k] = sum%10;
+            carry = sum/10;
+            i--;
+            k--;
+        }
+        while(j>=0){
+            sum =  arr2[j] + carry;
+            output[k] = sum%10;
+            carry = sum/10;
+            j--;
+            k--;
+        }
+        output[0] = carry;
+    }
+    public static void main(String[] args) {
+        int arr1[] = {9,7,6,1};
+        int arr2[] = {4,5,9};
+        int j1 = Math.max(arr1.length,arr2.length);
+        int output1[] = new int[j1+1];
+        sumOfTwoArrays(arr1,arr2,output1);
+        TakeInputAndPrint.printArray(output1);
+        System.out.println();
+        int arr3[] = {4};
+        int arr4[] = {6,9,8,5};
+        int j2 = Math.max(arr3.length,arr4.length);
+        int output2[] = new int[j1+1];
+        sumOfTwoArrays(arr3,arr4,output2);
+        TakeInputAndPrint.printArray(output2);
+    }
+}
