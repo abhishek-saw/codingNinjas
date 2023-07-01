353e310abf3e15a62f3f54908e8a1004c4df2f8f
ABHISHEK SAW <134182632+abhishek-saw@users.noreply.github.com>
2023-07-01 19:40:22 +0530

    OOPS I

---
+++ src/DSAInJAVA/OOPSTwo/StudentUse2.java
@@ -0,0 +1,17 @@
+package DSAInJAVA.OOPSTwo;
+
+import DSAInJAVA.OOPSOne.Student;
+
+import java.util.Scanner;
+
+//class of studentUse is created
+public class StudentUse2 {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        System.out.println(s1);
+//      s1.name = "Abhilasha";
+        s1.rollNumber = 1;
+//      System.out.println(s1.name);
+        System.out.println(s1.rollNumber);
+    }
+}
