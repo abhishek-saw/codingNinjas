94a3e2929e3f68e739037b71d93fb383d9fa098d
ABHISHEK SAW <134182632+abhishek-saw@users.noreply.github.com>
2023-08-29 06:22:25 +0530

    Linked List I

---
+++ src/DSAInJAVA/LinkedList/IncrementElement.java
@@ -0,0 +1,28 @@
+package DSAInJAVA.LinkedList;
+
+import static DSAInJAVA.LinkedList.LinkedListUse.printLinkedList;
+
+public class IncrementElement {
+    public static Node<Integer> createLinkedList() {
+        Node<Integer> n1 = new Node<>(10);
+        Node<Integer> n2 = new Node<>(20);
+        Node<Integer> n3 = new Node<>(30);
+        Node<Integer> n4 = new Node<>(40);
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        return n1;
+    }
+    private static void increment(Node<Integer> head) {
+        Node<Integer> temp = head;
+        while(temp!=null){
+            temp.data++;
+            temp = temp.next;
+        }
+    }
+    public static void main(String[] args) {
+        Node<Integer> head = createLinkedList();
+        increment(head);
+        printLinkedList(head);
+    }
+}
---
+++ src/DSAInJAVA/LinkedList/RemoveConsecutiveDuplicates.java
@@ -0,0 +1,25 @@
+package DSAInJAVA.LinkedList;
+
+import java.util.ArrayList;
+
+public class RemoveConsecutiveDuplicates {
+    public static void main(String[] args) {
+        int [] arr =  { 10,10,20,20,20,30,40,10};
+        ArrayList results = removeConsecutiveDuplicates(arr);
+        for(Object i : results){
+            System.out.println(i);
+        }
+    }
+
+    private static ArrayList<Integer> removeConsecutiveDuplicates(int[] arr) {
+        ArrayList<Integer> result = new ArrayList<>();
+        result.add(arr[0]);
+
+        for(int i = 1;i <arr.length;i++){
+            if(arr[i] != arr[i-1]){
+                result.add(arr[i]);;
+            }
+        }
+        return result;
+    }
+}
